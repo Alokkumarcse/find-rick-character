e3fa8a629f829033c6441e785abbc0a231f032b3
Alok Kumar <alokk13799@gmail.com>
2022-12-31 00:14:16 +0530

    fetch data using useEffect(callback, [dependencies]) method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, {useState, useEffect} from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+
 import Filter from "./components/filter/Filter";
 import Card from "./components/cards/Card";
+import { computeStyles } from "@popperjs/core";
 
 function App() {
+  // useState() hooks to manage the state, initially our pageNumber is "1" and fetchedData is [].
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fethcedData, updateFetchedData] = useState([]);
+
+  // Destructuring the fetchedData to get access of seprated data
+  let {info, results} = fethcedData;
+
+  // test here what we get as info and results
+  // console.log(info);
+  // console.log(results);
+
+  // Fetch data via making api call using useEffect() to perfrom the api call side effect
+  const baseUrl = 'https://rickandmortyapi.com/api';
+  let api = `${baseUrl}/character/?page=${pageNumber}`;
+  console.log(api);
+  
+  useEffect(() => {
+    (async function(){
+      let data = await fetch(api).then((response) => response.json());
+      updateFetchedData(data);
+    }())
+  }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
