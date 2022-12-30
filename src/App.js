018879687e647e9a71f95c601d26a5f12db6f205
Alok Kumar <alokk13799@gmail.com>
2022-12-30 17:19:42 +0530

    filter and character-card structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filter from "./components/filter/Filter";
+import Card from "./components/cards/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-4">
+        Find <span className="text-primary">Rick-Morty</span> Characters
+      </h1>
+
+      <div className="container border">
+        <div className="row border border-primary">
+          {/* Render filter component to show filter characters*/}
+            <div className="col-3">
+              <Filter />
+            </div>
+
+          {/* Render Card component to show all characters */}
+            <div className="col-8 border border-secondary">
+              <div className="row border border-success"> 
+                <Card />
+                <Card />
+                <Card />
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/cards/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='col-3'>
+      Card
+   </div>
+  )
+}
+
+export default Card
